5a663869ccf903e40fb30a5c410294735f2f37ba
Michael Gressier <michael.gressier@decathlon.com>
2017-11-24 16:12:25 +0100

    exercice 1

--- 02_programming_fundamentals/04_algorithms_day_2/01_range_function/01_range.js
+++ 02_programming_fundamentals/04_algorithms_day_2/01_range_function/01_range.js
@@ -4,9 +4,62 @@
 // This function should work in both ascending or descending order.
 
 // Complete this function.
-function range() {
+
+function range(startNumber,endNumber) {
+  const range2 = [];
+  if (startNumber < endNumber) {
+    for(let i = startNumber; i<=endNumber; i++) {
+      range2.push(i);
+    }
+  } else {
+    for(let i = startNumber; i>= endNumber; i--) {
+      range2.push(i);
+    }
+  }
+  return range2;
 
 }
+
+
+// function range_asc(min, max) {
+//   const data = [];
+//   for(let i = min; i <= max; i++) {
+//     data.push(i);
+//   }
+//   return data;
+// }
+//
+// function range_desc(max, min) {
+//   const data = [];
+//   for(let i = max; i >= min; i--) {
+//     data.push(i);
+//   }
+//   return data;
+// }
+//
+// function range(x, y) {
+//   let f;
+//   if (x < y) {
+//     return range_asc(x, y);
+//   } else {
+//     return range_desc(x, y);
+//   }
+// }
+//
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Do not remove last lines, it is for tests
 // eslint-disable-next-line
