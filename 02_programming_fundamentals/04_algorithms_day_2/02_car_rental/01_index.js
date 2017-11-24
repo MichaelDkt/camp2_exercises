a55bb65873772e0717300e9b1c1cb108ee732029
Michael Gressier <michael.gressier@decathlon.com>
2017-11-24 19:05:05 +0100

    exercice 24-11

--- 02_programming_fundamentals/04_algorithms_day_2/02_car_rental/01_index.js
+++ 02_programming_fundamentals/04_algorithms_day_2/02_car_rental/01_index.js
@@ -9,9 +9,14 @@ const driver = {
 // * Input: a driver
 // * Output: a boolean if the driver can rent a car
 
+function canRentACar(driver) {
+  const licenseOK = ["B", "B1"].includes(driver.driverLicense);
+  const dateOK = driver.licenseIssued <= new Date().getFullYear() - 2;
+  const accidentsOK = driver.numberOfAccident === 0;
+  const bonusOK = driver.bonus >= 0.7;
 
-
-
+  return licenseOK && dateOK && (accidentsOK || bonusOK);
+}
 // ⚠ Do not remove me ! It's for tests
 // eslint-disable-next-line
 module.exports = canRentACar
