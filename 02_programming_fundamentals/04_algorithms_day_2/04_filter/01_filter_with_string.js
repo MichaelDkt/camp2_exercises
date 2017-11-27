c35dce78521abd7cecab02c46e2989d961a03f81
Michael Gressier <michael.gressier@decathlon.com>
2017-11-27 15:35:00 +0100

    maj 27-11 à 15h

--- 02_programming_fundamentals/04_algorithms_day_2/04_filter/01_filter_with_string.js
+++ 02_programming_fundamentals/04_algorithms_day_2/04_filter/01_filter_with_string.js
@@ -1,18 +1,19 @@
 // filter takes an array of integer and a string with either odd or even
 // such as filter([1, 2, 3, 4, 5], 'even') returns [2, 4]
-function filter(number) {
-  const oddOrEvenFilter = [];
 
-  for(let i = 0; i < number.length; i++) {
-    if (number[i] === 1 || number[i] === 3 || number[i] === 5) {
-      oddOrEvenFilter.push(number[i], "odd");
+const twoOrFour = [];
 
-    } else if (number[i] === 2 || number[i] === 4) {
-      oddOrEvenFilter.push(number[i], "even");
-    }
+for(let i = 0; i <= 5; i++) {
+  if (twoOrFour[i] === 2 || twoOrFour[i] === 4) {
+    twoOrFour.push[i];
   }
-  return (oddOrEvenFilter);
 }
+console.log = twoOrFour.push(1,2,3,4,5);
+
+
+
+
+
 // do not remove this line, it is for tests
 module.exports = filter;
 
@@ -20,3 +21,17 @@ module.exports = filter;
 // Target
 //const oddArray = ([1, 3, 5], "odd");
 //const evenArray = ([2, 4], "even");
+
+//function filter(number) {
+//  const oddOrEvenFilter = [];
+
+// for(let i = 0; i < number.length; i++) {
+//    if (number[i] === 1 || number[i] === 3 || number[i] === 5) {
+//      oddOrEvenFilter.push(number[i], "odd");
+
+//    } else if (number[i] === 2 || number[i] === 4) {
+//      oddOrEvenFilter.push(number[i], "even");
+//    }
+//  }
+//  return (oddOrEvenFilter);
+//}
--- 02_programming_fundamentals/04_algorithms_day_2/04_filter/02_filter_with_function.js
+++ 02_programming_fundamentals/04_algorithms_day_2/04_filter/02_filter_with_function.js
@@ -1,8 +1,11 @@
 // filter takes an array of integer and a function of filtering
 // such as filter([1, 2, 3, 4, 5], pickEvenNumbers) returns [2, 4]
-function filter(array, fn) {
-  // Your code here
+function odd(x) {
+  return (x % 2) === 0;
 }
+
+const x = [1, 2, 3, 4, 5].filter(odd);
+x === [2, 4];
 
 // do not remove this line, it is for tests
 module.exports = filter;
