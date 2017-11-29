1aa1e3ae6b4ab02b302da5ada66128d20fe4587a
Michael Gressier <michael.gressier@decathlon.com>
2017-11-29 13:57:30 +0100

    maj 29/11 à 14h

--- 02_programming_fundamentals/03_algorithms_day_1/04_play_with_for_loop/09_filtering_with_while.js
+++ 02_programming_fundamentals/03_algorithms_day_1/04_play_with_for_loop/09_filtering_with_while.js
@@ -3,12 +3,10 @@
 // -  Create an array called `litteralDigits` from `zero` to `nine` where each array entry is a spelled-out number;
 // -  Using `length`, write on `stdout` each odd values of the table.
 
-const litteralDigits = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"].filter(odd);
+let litteralDigits = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+let i = 1;
 
-for(let i = 0 ; i < litteralDigits.length; i++) {
+while(i < litteralDigits.length) {
   console.log(litteralDigits[i]);
-}
-
-function odd(litteralDigits) {
-  return (litteralDigits % 2) !== 0;
+  i+=2;
 }
