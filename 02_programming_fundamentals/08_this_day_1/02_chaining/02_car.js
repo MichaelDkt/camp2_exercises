2a5f7c352bc1e8983718fa9265b0c6c86b7721c3
Michael Gressier <michael.gressier@decathlon.com>
2017-11-30 14:10:36 +0100

    maj 30/11 à 14h00

--- 02_programming_fundamentals/08_this_day_1/02_chaining/02_car.js
+++ 02_programming_fundamentals/08_this_day_1/02_chaining/02_car.js
@@ -1,3 +1,32 @@
 // See Sparta courses for the exercise summary
+const car = {
+  speed : 0,
+  time : 0,
+  distance : 0,
+  newDistance : 0,
+
+  start : function() {
+    this.speed = 0;
+    this.time = 0;
+    this.distance = 0;
+    this.newDistance = 0;
+    return this;
+  },
+  changeSpeed : function(speed) {
+    this.speed = speed;
+    return this;
+  },
+  drive : function(time) {
+    this.time = time;
+    this.distance = (this.speed/60) * time;
+    this.newDistance = this.distance + this.newDistance;
+    return this;
+  },
+  showDistance : function() {
+    console.log(`${this.newDistance} Km`);
+  }
+}
+
+car.start().changeSpeed(130).drive(42).showDistance();
 
 module.exports = car;
