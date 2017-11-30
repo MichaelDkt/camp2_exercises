eed9acd5f456f9e1aa9226786d7ea53f12566e34
Michael Gressier <michael.gressier@decathlon.com>
2017-11-30 14:56:19 +0100

    maj 30-11 à 15h

--- 02_programming_fundamentals/08_this_day_1/03_implementing/03_coffee_machine.js
+++ 02_programming_fundamentals/08_this_day_1/03_implementing/03_coffee_machine.js
@@ -1,5 +1,29 @@
 // See Sparta courses for the exercise summary
 
+const machine = {
+  litersOfCoffee : 0.00,
+
+  expresso : function() {
+    if (this.litersOfCoffee >= 0.08) {
+      this.litersOfCoffee = this.litersOfCoffee - 0.08;
+      return true;
+    } else {
+      return false;
+    }
+  },
+  longCoffee : function() {
+    if (this.litersOfCoffee >= 0.05) {
+      this.litersOfCoffee = this.litersOfCoffee - 0.15;
+      return true;
+    } else {
+      return false;
+    }
+  },
+  fillWithLitersOfCoffee : function(liter) {
+      return this.litersOfCoffee = this.litersOfCoffee + liter;
+  }
+}
+
 // Coffee Machine usage. Insert your code above this comment
 
 machine.fillWithLitersOfCoffee(10);
@@ -10,5 +34,6 @@ console.log(machine.litersOfCoffee) // => 9.77
 // some more people ordering coffee here
 console.log(machine.litersOfCoffee) // => 0.02
 console.log(machine.expresso()) // => false
+
 
 module.exports = machine;
