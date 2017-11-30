ac20db7d692e10ff6489bbfe5179439de4a1d8f8
Michael Gressier <michael.gressier@decathlon.com>
2017-11-30 18:27:21 +0100

    maj 30/11 à 18h00

--- 02_programming_fundamentals/08_this_day_1/04_bonus/04_orange_tree.js
+++ 02_programming_fundamentals/08_this_day_1/04_bonus/04_orange_tree.js
@@ -1,3 +1,62 @@
 // See Sparta courses for the exercise summary
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const orangeTree = {
+  height : 0,
+  age : 0,
+  oranges : 0,
+  alive : false,
+  ageDie : randomInt(50, 100),
+
+  ageOneYear : function() {
+    this.age = this.age + 1;
+
+    if (this.age < 10) {
+       this.height = this.age * 25;
+    } else if (this.age >= 10 && this.age < 20) {
+       this.height = (9*25) + (this.age-9) * 10;
+    } else if (this.age > 20 && this.age <= 100) {
+      this.height = (9*25 + 10*10);
+    }
+
+    if (this.age  >= 5 && this.age < 10) {
+      this.oranges = 10;
+    } else if (this.age >= 10 && this.age < 20) {
+       this.oranges = 20;
+    } else if (this.age >=20 && this.age < 40) {
+       this.oranges = 5;
+    } else {
+       this.oranges = 0;
+    }
+
+      if ((this.age >= 50 && this.age === this.ageDie) || this.age >= 100) {
+        this.alive = false;
+      } else {
+        this.alive = true;
+      }
+      return this;
+  },
+  pickAnOrange : function() {
+    if (this.oranges > 0) {
+      this.oranges = this.oranges - 1;
+      return true;
+    } else {
+      return false;
+    }
+  },
+  seed : function() {
+    this.height = 0;
+    this.age = 0;
+    this.oranges = 0;
+    this.alive = true;
+
+  }
+}
+
+
+
+
 
 module.exports = orangeTree;
