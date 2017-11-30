f21ca9621307405a6154157051aa6ecfd3118c60
Michael Gressier <michael.gressier@decathlon.com>
2017-11-30 11:01:49 +0100

    maj 30-11

--- 02_programming_fundamentals/03_algorithms_day_1/05_play_with_for_loop_bonus/05_pyramid.js
+++ 02_programming_fundamentals/03_algorithms_day_1/05_play_with_for_loop_bonus/05_pyramid.js
@@ -6,3 +6,16 @@
 //  *****
 // *******
 // ```
+
+let fullLine = ["*", "*", "*", "*", "*", "*", "*"];
+
+let startSpace = 0;
+let endSpace = 7;
+
+while (fullLine !== ["", "", "", "*", "", "", ""]) {
+
+  (fullLine[startSpace]) = "" && (fullLine[endSpace] = "") && (startSpace = startSpace + 1) && (endSpace = endSpace - 1);
+
+  console.log(fullLine.join(""));
+
+}
