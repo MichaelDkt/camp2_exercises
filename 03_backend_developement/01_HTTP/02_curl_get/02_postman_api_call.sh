756d459b098ffd5898a71557fd410c77713a791a
Michael Gressier <michael.gressier@decathlon.com>
2017-12-04 15:03:56 +0100

    maj 04/12 à 15h

---
+++ 03_backend_developement/01_HTTP/02_curl_get/02_postman_api_call.sh
@@ -0,0 +1,1 @@
+curl get https://postman-echo.com/get?foo=bar | jq .args > 02_postman_api_call.result
---
+++ 03_backend_developement/01_HTTP/02_curl_get/05_postman_api_call.sh
@@ -0,0 +1,1 @@
+curl "https://postman-echo.com/time/valid"?todate=2017-12-04 > 05_postman_api_call.result
