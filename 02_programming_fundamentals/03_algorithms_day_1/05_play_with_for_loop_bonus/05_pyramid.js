fb6b8788ff539d0b6c580138fee24de60fda557d
Michael Gressier <michael.gressier@decathlon.com>
2017-12-05 11:43:52 +0100

    maj 05/11 à 9h

--- 02_programming_fundamentals/03_algorithms_day_1/05_play_with_for_loop_bonus/05_pyramid.js
+++ 02_programming_fundamentals/03_algorithms_day_1/05_play_with_for_loop_bonus/05_pyramid.js
@@ -7,15 +7,14 @@
 // *******
 // ```
 
-let fullLine = ["*", "*", "*", "*", "*", "*", "*"];
+const space = ["", "", "", ""];
+const space2 = ["", "", "", ""];
+const star = ["*"];
 
-let startSpace = 0;
-let endSpace = 7;
-
-while (fullLine !== ["", "", "", "*", "", "", ""]) {
-
-  (fullLine[startSpace]) = "" && (fullLine[endSpace] = "") && (startSpace = startSpace + 1) && (endSpace = endSpace - 1);
-
-  console.log(fullLine.join(""));
-
+const pyramid = [];
+for (let i = 0; i <7; i++) {
+  pyramid.push(space.pop());
+  pyramid.push(star);
+  pyramid.push(space2.pop());
+  console.log(pyramid.join());
 }
