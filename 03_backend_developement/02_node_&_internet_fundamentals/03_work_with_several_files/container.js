cc32542dff7a2a2d4255838ba5a9b855b110dae2
Michael Gressier <michael.gressier@decathlon.com>
2017-12-06 09:13:29 +0100

    maj 06-12 à 9h

---
+++ 03_backend_developement/02_node_&_internet_fundamentals/03_work_with_several_files/container.js
@@ -0,0 +1,21 @@
+const container = {
+  litersOfCoffee : 0,
+};
+
+function putLitersOfCoffee(numberOfLiters) {
+  container.litersOfCoffee += numberOfLiters;
+}
+
+function consumeLitersOfCoffee(numberOfLiters) {
+  if (container.litersOfCoffee >= numberOfLiters) {
+    container.litersOfCoffee -= numberOfLiters;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+module.exports = {
+  putLitersOfCoffee : putLitersOfCoffee,
+  consumeLitersOfCoffee : consumeLitersOfCoffee,
+};
