8643e0455ef42231f39efcde8e5fab7f63babdca
MICHAEL GRESSIER <michael.gressier@decathlon.com>
2018-01-16 15:36:17 +0100

    correction slacky

--- 06_react_redux/06_websocket/slacky/src/App.js
+++ 06_react_redux/06_websocket/slacky/src/App.js
@@ -1,29 +1,69 @@
-import React, { Component } from 'react';
-import './App.css';
-
+import React, { Component } from "react";
+import "./App.css";
+import Login from "./Login";
+import Chat from "./Chat";
 
 class App extends Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: null,
+      messages: [],
+    };
+    // Attaching the websocket to our App so we can reuse it
+    this.websocket = new WebSocket("ws://localhost:8080");
+  }
   componentDidMount() {
-       const nbUsersElem = document.getElementById("nbUsers");
-       const ws = new WebSocket("ws://localhost:4000");
-       ws.onmessage = (event) => {
-         console.log("Message: " + event.data);
-        nbUsersElem.innerHTML = event.data;
-       };
-       window.addEventListener("beforeunload", () => ws.send("CLOSE"));
-     }
+    // Listen for messages
+    this.websocket.addEventListener("message", event => {
+      const message = JSON.parse(event.data);
+      console.log("Message from server ", message);
+      switch (message.type) {
+        case "CONNECTION_START":
+        default:
+          return;
+        case "MESSAGES":
+          this.setState({ messages: message.data });
+          return;
+      }
+    });
+  }
+
+  handleUserName = userName => {
+    this.setState({ userName: userName });
+    this.websocket.send(
+      JSON.stringify({
+        type: "LOGIN",
+        userName: userName
+      })
+    );
+  };
+
+  sendMessage = message => {
+    this.websocket.send(
+      JSON.stringify({
+        type: "NEW_MESSAGE",
+        userName: this.state.userName,
+        message: message
+      })
+    );
+  };
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to SLACKY</h1>
-          <p>There is <span id="nbUsers">0</span> users connected.</p>
+          <h1 className="App-title">Slacky</h1>
         </header>
+
+        {this.state.userName ? (
+          <Chat sendMessage={this.sendMessage} messages={this.state.messages} />
+        ) : (
+          <Login handleUserName={this.handleUserName} />
+        )}
       </div>
-      );
-    }
+    );
+  }
 }
 
-  export default App;
+export default App;
